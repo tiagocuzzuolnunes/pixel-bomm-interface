9a252af2551c2528c1c7448af7bd18e616e7628e
tiagocuzzuolnunes <tiagocuzzuolnunes@gmail.com>
2025-05-12 16:12:06 -0300

    feat: initialize page content configuration

---
+++ src/app/components/PageContent.tsx
@@ -0,0 +1,4 @@
+import { Button } from "@/components/ui/button";
+import { Dialog } from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+export default function PageContent() {}
